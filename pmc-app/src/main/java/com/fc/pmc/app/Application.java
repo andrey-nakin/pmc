f4b23a5dd68d89c6b160f78e96f8527187731719
Andrey Nakin <andrey@ter-net.com>
2020-11-09 17:58:04 +0300

    work

---
+++ pmc-app/src/main/java/com/fc/pmc/app/Application.java
@@ -0,0 +1,17 @@
+package com.fc.pmc.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author Andrey Nakin
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
